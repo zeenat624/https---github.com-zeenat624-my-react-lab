599d7f69dec8bce90797b854683143cf05b8ef68
zeenat624 <zeenatmhanif624@gmail.com>
2024-12-03 22:30:38 -0800

    lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
+import UserInfo from './Components/UserInfo';
 
 function App() {
-  const User_Data = {
+  const User_Data1 = {
     name: "Muhammad Mashhood",
     email: "mashhoodpetro17@gmail.com",
     role: "Head of college",
-    ProfilePic: "ProfilePic.jpg"
+    ProfilePic: <img className= 'image' src="ProfilePic.jpg"/>
   }
   return (
     <div className="App">
-  
+    <UserInfo User_Data={User_Data1}/>
     </div>
   );
 }
