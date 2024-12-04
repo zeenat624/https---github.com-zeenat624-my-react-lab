0700cbb929d27013beecd13cc0f1f4d5a8c47696
zeenat624 <zeenatmhanif624@gmail.com>
2024-12-03 23:01:33 -0800

    lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
+import './styles/UserInfo.css';
 import './App.css';
 import UserInfo from './Components/UserInfo';
 
 function App() {
   const User_Data1 = {
+    ProfilePic: <img className= 'image' src="ProfilePic.jpg"/>,
     name: "Muhammad Mashhood",
-    email: "mashhoodpetro17@gmail.com",
-    role: "Head of college",
-    ProfilePic: <img className= 'image' src="ProfilePic.jpg"/>
+    email:<a href="url">mashhoodpetro17@gmail.com</a> ,
+    role: "Head of college"
+  }
+  const Statistics_Data1 = {
+    totalUser: "100",
+    activeUsers: "70",
+    newSignups: "10",
+    acivePercentage: (70*10/ 100)*100,
   }
   return (
-    <div className="App">
+    <div className="container">
     <UserInfo User_Data={User_Data1}/>
     </div>
   );
--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,10 +1,11 @@
+import '../styles/UserInfo.css';
 function UserInfo ({User_Data}){
     return(
-        <div>
+        <div >
+        <p>{User_Data.ProfilePic}</p>
         <h2>{User_Data.name}</h2>
         <p>{User_Data.email}</p>
-        <p>{User_Data.role}</p>
-        <p>{User_Data.ProfilePic}</p>
+        <h3>{User_Data.role}</h3>
         </div>
     )
 }
